0039d80a2c39275943043eccb2c12e333700901f
Webster <webster.jorgensen@gmail.com>
2022-10-11 11:55:47 -0500

    first commit

---
+++ strapi/config/env/production/database.js
@@ -0,0 +1,24 @@
+// path: ./config/env/production/database.js
+
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
+module.exports = ({ env }) => ({
+  connection: {  
+    client: 'postgres',
+    connection: {
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+      debug: false,
+  },
+});
