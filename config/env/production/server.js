275dfdf5f7fa3bb314b93fa2258f7d3bde94ecdc
Webster <webster.jorgensen@gmail.com>
2022-10-25 10:06:04 -0500

    updated server.js config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
+// path: ./config/env/production/server.js
 
-module.exports = ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  // port: env.int('PORT', 1337),
-  url: env('APP_URL'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  module.exports = ({ env }) => ({
+    proxy: true,
+    url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+    app: { 
+      keys: env.array('APP_KEYS')
+    },
 });
