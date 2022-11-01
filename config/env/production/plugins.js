733585585ac57d480c1809883272aff5349fcb86
Webster <webster.jorgensen@gmail.com>
2022-11-01 14:37:37 -0500

    debugging do spaces config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'strapi-provider-upload-aws-s3',
       providerOptions: {
         accessKeyId: 'DO00DHVJDTD2XXGL984Z',
         secretAccessKey: 'LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows',
