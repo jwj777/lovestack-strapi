725ea45ce22bec0c7866542030a9d7bf899fbfbd
Webster <webster.jorgensen@gmail.com>
2022-11-01 13:22:12 -0500

    do spaces config

---
+++ config/env/development/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: 'DO00DHVJDTD2XXGL984Z',
+        secretAccessKey: 'LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows',
+        region: 'nyc3',
+        endpoint: 'nyc3.digitaloceanspaces.com',
+        params: {
+          Bucket: 'lovestack',
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
