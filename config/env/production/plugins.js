1053e670623f9cb947ac5a765b2f27397364c1be
Webster <webster.jorgensen@gmail.com>
2022-11-01 21:17:51 -0500

    reinstalled node modules to fix aws-s3 provider error

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: 'DO00DHVJDTD2XXGL984Z',
-        secretAccessKey: 'LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows',
-        region: 'nyc3',
-        endpoint: 'nyc3.digitaloceanspaces.com',
+        accessKeyId: env('DO00DHVJDTD2XXGL984Z'),
+        secretAccessKey: env('LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows'),
+        region: env('nyc3'),
+        endpoint: env('nyc3.digitaloceanspaces.com'),
         params: {
-          Bucket: 'lovestack',
+          Bucket: env('lovestack'),
         },
       },
       actionOptions: {
