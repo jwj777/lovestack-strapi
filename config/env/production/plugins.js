8560db47cb6756e7a6a336ac0ef97d466c5e3771
Webster <webster.jorgensen@gmail.com>
2022-11-04 01:55:56 -0500

    put keys back in plugins

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,13 +1,11 @@
-import { secretAccessKey, accessKeyId } from '/resources.js';
-
 module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env(resources.accessKeyId),
-        secretAccessKey: env(resources.secretAccessKey),
+        accessKeyId: env('DO00DHVJDTD2XXGL984Z'),
+        secretAccessKey: env('LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows'),
         region: env('nyc3'),
         endpoint: env('nyc3.digitaloceanspaces.com'),
         params: {
