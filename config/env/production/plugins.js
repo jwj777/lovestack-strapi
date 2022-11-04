980da0cf27461528777c40c875849df08784bd49
Webster <webster.jorgensen@gmail.com>
2022-11-04 13:51:49 -0500

    provider update

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'strapi-provider-upload-aws-s3',
+      provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
