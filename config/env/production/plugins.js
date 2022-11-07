6342561715fe1936d90e520db51207abf46e3128
Webster <webster.jorgensen@gmail.com>
2022-11-07 13:34:55 -0600

    more provider config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,13 +1,37 @@
+// module.exports = ({ env }) => ({
+//   // ...
+//   upload: {
+//     config: {
+//       provider: 'strapi-provider-upload-aws-s3',
+//       providerOptions: {
+//         accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//         secretAccessKey: env('AWS_ACCESS_SECRET'),
+//         params: {
+//           Bucket: env('AWS_BUCKET'),
+//         },
+//       },
+//       actionOptions: {
+//         upload: {},
+//         uploadStream: {},
+//         delete: {},
+//       },
+//     },
+//   },
+//   // ...
+// });
+
 module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'strapi-provider-upload-aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        accessKeyId: 'DO00DHVJDTD2XXGL984Z',
+        secretAccessKey: 'LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows',
+        region: 'nyc3',
+        endpoint: 'nyc3.digitaloceanspaces.com',
         params: {
-          Bucket: env('AWS_BUCKET'),
+          Bucket: 'lovestack',
         },
       },
       actionOptions: {
