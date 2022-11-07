0a12ef6c43987d56b49c43bbc2e720edf5a33012
Webster <webster.jorgensen@gmail.com>
2022-11-07 14:00:22 -0600

    switching to AWS S3 for images

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -20,26 +20,26 @@
 //   // ...
 // });
 
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-aws-s3',
-      providerOptions: {
-        accessKeyId: 'DO00DHVJDTD2XXGL984Z',
-        secretAccessKey: 'LYAJdCqm798OA0s7F75uNXTx+syLSbjTFr+x7129ows',
-        region: 'nyc3',
-        endpoint: 'nyc3.digitaloceanspaces.com',
-        params: {
-          Bucket: 'lovestack',
+    // ~/strapi-aws-s3/backend/config/plugins.js
+    
+    module.exports = ({ env }) => ({
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+              Bucket: env('AWS_BUCKET'),
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-  // ...
-});
+    });
+    
