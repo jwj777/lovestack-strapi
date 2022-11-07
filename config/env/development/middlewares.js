3357ac9d19b6ca348c8f9bcada6a24ef1fa7e15d
Webster <webster.jorgensen@gmail.com>
2022-11-07 13:58:59 -0600

    switching to AWS S3 for images

--- config/env/development/middlewares.js
+++ config/env/development/middlewares.js
@@ -1,26 +1,67 @@
+// module.exports = [
+//   // ...
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+//           "media-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   // ...
+// ];
+
+
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
+// ~/strapi-aws-s3/backend/config/middlewares.js
+    
 module.exports = [
-  // ...
+  'strapi::errors',
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
-          "media-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-aws-s3-images-bucket-2.s3.us-east-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-aws-s3-images-bucket-2.s3.us-east-2.amazonaws.com', 
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  // ...
-];
-
-
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
