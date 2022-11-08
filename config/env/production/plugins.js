a162bd4a57ac58fca5ddbe5727ad868e2e612dca
Webster <webster.jorgensen@gmail.com>
2022-11-08 08:56:48 -0600

    renamed provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -25,7 +25,7 @@
     module.exports = ({ env }) => ({
       upload: {
         config: {
-          provider: 'aws-s3',
+          provider: 'provider-upload-aws-s3',
           providerOptions: {
             accessKeyId: env('AWS_ACCESS_KEY_ID'),
             secretAccessKey: env('AWS_ACCESS_SECRET'),
@@ -42,4 +42,3 @@
         },
       },
     });
-    
