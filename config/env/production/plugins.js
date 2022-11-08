a69ec389f69a07ed6fe5dfcd730950048838570f
Webster <webster.jorgensen@gmail.com>
2022-11-08 15:58:44 -0600

    ...

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,44 +1,42 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   upload: {
-//     config: {
-//       provider: 'strapi-provider-upload-aws-s3',
-//       providerOptions: {
-//         accessKeyId: env('AWS_ACCESS_KEY_ID'),
-//         secretAccessKey: env('AWS_ACCESS_SECRET'),
-//         params: {
-//           Bucket: env('AWS_BUCKET'),
-//         },
-//       },
-//       actionOptions: {
-//         upload: {},
-//         uploadStream: {},
-//         delete: {},
-//       },
-//     },
-//   },
-//   // ...
-// });
-
-    // ~/strapi-aws-s3/backend/config/plugins.js
-    
-    module.exports = ({ env }) => ({
-      upload: {
-        config: {
-          provider: 'aws-s3',
-          providerOptions: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
-            region: env('AWS_REGION'),
-            params: {
-              Bucket: env('AWS_BUCKET'),
-            },
-          },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('DO_ACCESS_KEY_ID'),
+        secretAccessKey: env('DO_ACCESS_SECRET'),
+        endpoint: env('DO_ENDPOINT'),
+        params: {
+          Bucket: env('DO_BUCKET'),
         },
       },
-    });
+    },
+  },
+  // ...
+});
+
+
+// //  S3 Version
+//     // ~/strapi-aws-s3/backend/config/plugins.js
+    
+//     module.exports = ({ env }) => ({
+//       upload: {
+//         config: {
+//           provider: 'aws-s3',
+//           providerOptions: {
+//             accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//             secretAccessKey: env('AWS_ACCESS_SECRET'),
+//             region: env('AWS_REGION'),
+//             params: {
+//               Bucket: env('AWS_BUCKET'),
+//             },
+//           },
+//           actionOptions: {
+//             upload: {},
+//             uploadStream: {},
+//             delete: {},
+//           },
+//         },
+//       },
+//     });
