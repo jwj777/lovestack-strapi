87903ba7043cb9e1abe2f88df0937e3f23ba4ae6
Webster <webster.jorgensen@gmail.com>
2022-11-08 23:19:53 -0600

    changed middleware

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,43 +1,43 @@
 
-module.exports = [
-  'strapi::errors',
-  {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              '*.nyc3.digitaloceanspaces.com',
-              'nyc3.digitaloceanspaces.com',
-            ],
-            'media-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              '*.nyc3.digitaloceanspaces.com',
-              'nyc3.digitaloceanspaces.com',
-            ],
-            upgradeInsecureRequests: null,
-          },
-        },
-      },
-    },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+// module.exports = [
+//   'strapi::errors',
+//   {
+//       name: 'strapi::security',
+//       config: {
+//         contentSecurityPolicy: {
+//           useDefaults: true,
+//           directives: {
+//             'connect-src': ["'self'", 'https:'],
+//             'img-src': [
+//               "'self'",
+//               'data:',
+//               'blob:',
+//               'dl.airtable.com',
+//               '*.nyc3.digitaloceanspaces.com',
+//               'nyc3.digitaloceanspaces.com',
+//             ],
+//             'media-src': [
+//               "'self'",
+//               'data:',
+//               'blob:',
+//               'dl.airtable.com',
+//               '*.nyc3.digitaloceanspaces.com',
+//               'nyc3.digitaloceanspaces.com',
+//             ],
+//             upgradeInsecureRequests: null,
+//           },
+//         },
+//       },
+//     },
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 
 
 // ~/strapi-aws-s3/backend/config/middlewares.js
@@ -79,3 +79,38 @@ module.exports = [
 //   'strapi::favicon',
 //   'strapi::public',
 // ];
+
+
+module.exports = [
+  // ...
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+          "media-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // ...
+];
+
+
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
