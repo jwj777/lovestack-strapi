46167215aa2f74c70762287fe0248128be59e59e
Webster <webster.jorgensen@gmail.com>
2023-03-09 00:34:27 -0600

    removed is owner updates

--- src/api/company/controllers/company.js
+++ src/api/company/controllers/company.js
@@ -6,39 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreController('api::company.company');
-
-module.exports = createCoreController('api::company.company', ({strapi}) => ({
-
-  async me(ctx, next) {
-    const user = ctx.state.user
-    if (!user) {
-      return ctx.badRequest(null, [{messages: [{id: "No auth header found"}]}])
-    }
-
-    const data = await strapi.entityService.findMany("api::company.company", {
-      populate: 'image',
-      filters: {
-        "owner": {
-          "id": user.id
-        }
-      },
-
-    });
-    if (!data) {
-      return ctx.notFound();
-    }
-
-    const sanitizedEvents = await this.sanitizeOutput(data, ctx);
-
-    return this.transformResponse(sanitizedEvents);
-  },
-  async create(ctx) {
-    const {id} = ctx.state.user; //ctx.state.user contains the current authenticated user
-    const response = await super.create(ctx);
-    const updatedResponse = await strapi.entityService
-      .update('api::company.company', response.data.id, {data: {owner: id}})
-    return updatedResponse;
-  },
-  
-}));
+module.exports = createCoreController('api::company.company');
--- src/api/company/routes/company.js
+++ src/api/company/routes/company.js
@@ -6,13 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::company.company', {
-  config: {
-    update: {
-        "policies" : ["is-owner"]
-    },
-    delete: {
-        "policies" : ["is-owner"]
-    }
-  }
-});
+module.exports = createCoreRouter('api::company.company');
