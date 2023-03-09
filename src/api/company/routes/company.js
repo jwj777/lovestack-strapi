9f400597c1912e84e39716aaf0d672872149d6e3
Webster <webster.jorgensen@gmail.com>
2023-03-08 23:42:21 -0600

    is owner policy

--- src/api/company/routes/company.js
+++ src/api/company/routes/company.js
@@ -6,4 +6,13 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::company.company');
+module.exports = createCoreRouter('api::company.company', {
+  config: {
+    update: {
+        "policies" : ["is-owner"]
+    },
+    delete: {
+        "policies" : ["is-owner"]
+    }
+  }
+});
