9ae854464000294c8521d5603f77ba48176ba9ae
Webster <webster.jorgensen@gmail.com>
2023-08-07 12:53:00 -0500

    general

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,24 +1,52 @@
+
+// module.exports = [
+//   // ...
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+//           "media-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   // ...
+// ];
+
 module.exports = [
-  // ...
+  'strapi::errors',
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
-          "media-src": ["'self'", "data:", "blob:", `${"https://lovestack.nyc3.digitaloceanspaces.com"}`],
-          upgradeInsecureRequests: null,
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.digitaloceanspaces.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '*.digitaloceanspaces.com',
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  // ...
-];
 
-module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
