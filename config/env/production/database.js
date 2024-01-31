3c2a63280dfbe23719351fc21ba62e8791a7199c
Webster Jorgensen <webster.jorgensen@gmail.com>
2024-01-31 08:31:44 -0600

    prod db updates

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,44 +1,44 @@
 // path: ./config/env/production/database.js
 
-// const parse = require("pg-connection-string").parse;
+const parse = require("pg-connection-string").parse;
 
-// const { host, port, database, user, password } = parse(
-//   process.env.DATABASE_URL
-// );
-
-
-// module.exports = ({ env }) => ({
-//   connection: {  
-//     client: 'postgres',
-//     connection: {
-//       host,
-//       port,
-//       database,
-//       user,
-//       password,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     },
-//       debug: false,
-//   },
-// });
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
 
 
 module.exports = ({ env }) => ({
-  connection: {
+  connection: {  
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized: false,
       },
     },
-    debug: false,
+      debug: false,
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST'),
+//       port: env.int('DATABASE_PORT'),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       ssl: {
+//         rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+//       },
+//     },
+//     debug: false,
+//   },
+// });
 
